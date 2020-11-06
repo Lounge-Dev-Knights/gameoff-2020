74225a6cde16c2de7450b4a75e6257918ce1fee5
Dardan Bujupaj <d.bujupaj@bluewin.ch>
2020-11-06 20:53:32 +0100

    move level code to baselevel class from which the playable level and the editor level can be derived of.

---
+++ scenes/BaseLevel.gd
@@ -0,0 +1,67 @@
+extends Node2D
+
+
+onready var moon = $Moon
+onready var black_hole = $BlackHole
+onready var stars = $stars
+onready var objects = $objects
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func save_level() -> Dictionary:
+	get_class()
+	return {}
+
+
+func load_level(level_data: Dictionary) -> void:
+	load_objects(level_data["objects"])
+	load_stars(level_data["stars"])
+	
+	var hole_data = level_data["black_hole"]
+	black_hole.position = Vector2(hole_data["pos_x"], hole_data["pos_y"])
+
+
+func add_object(type: String, pos: Vector2 = Vector2(0, 0)) -> Node2D:
+	var instance
+	
+	match type:
+		"Planet", _:
+			instance = preload("res://scenes/objects/Planet.tscn").instance()
+	
+	instance.position = pos
+	
+	objects.add_child(instance)
+	
+	return instance
+
+
+func load_objects(objects_data: Array) -> void:
+	var objects = $objects
+	
+	# remove existing
+	for o in objects.get_children():
+		o.queue_free()
+	
+	for object in objects_data:
+		add_object(object["type"], Vector2(object["pos_x"], object["pos_y"]))
+
+
+func add_star(pos: Vector2 = Vector2(0, 0)) -> Node2D:
+	var instance = preload("res://scenes/Star.tscn").instance()
+	instance.position = pos
+	stars.add_child(instance)
+	return instance
+
+
+
+func load_stars(stars_data: Array) -> void:
+	var stars = $stars
+	
+	for s in stars.get_children():
+		s.queue_free()
+	
+	for star in stars_data:
+		add_star(Vector2(star["pos_x"], star["pos_y"]))
--- scenes/level_editor/LevelEditor.gd
+++ scenes/level_editor/LevelEditor.gd
@@ -1,18 +1,25 @@
 extends Node2D
 
 
+const USER_LEVELS_PATH = "user://levels/"
+
+
 onready var stars = $Level/stars
 onready var objects = $Level/objects
-onready var camera = $Level/Camera2D
+onready var camera = $Camera2D
+onready var level = $BaseLevel
+
+onready var level_name = $CanvasLayer/PanelContainer/VBoxContainer/VBoxContainer/LevelName
 
 
 # node that is beeing dragged
 var drag_object: Node2D
+var selected_objects: Array = []
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	level.get_node("BlackHole").add_to_group("draggable")
 
 
 func _physics_process(delta):
@@ -26,10 +33,13 @@ func _physics_process(delta):
 func _unhandled_input(event):
 	if Input.is_action_just_released("drag"):
 		_stop_drag()
+		
 	if Input.is_action_just_pressed("zoom_in"):
-		camera.zoom *= 0.9
+		var position = (event as InputEventMouseButton).position
+		zoom_at_point(0.9, position)
 	if Input.is_action_just_pressed("zoom_out"):
-		camera.zoom *= 1.1
+		var position = (event as InputEventMouseButton).position
+		zoom_at_point(1.1, position)
 
 
 # check if cursor points to a node and start dragging
@@ -37,16 +47,61 @@ func _start_drag():
 	var state = get_world_2d().direct_space_state
 	var intersections = state.intersect_point(get_global_mouse_position(), 32, [], 0x7FFFFFFF, true, true)
 	for intersection in intersections:
-		drag_object = intersection["collider"]
+		var collider: Node2D = intersection["collider"]
+		if collider.is_in_group("draggable"):
+			drag_object = collider
+
 
 func _stop_drag():
 	drag_object = null
 
 
+func zoom_at_point(zoom_change, point):
+		var c0 = camera.global_position # camera position
+		var v0 = camera.get_viewport().size # vieport size
+		var c1 # next camera position
+		var z0 = camera.zoom # current zoom value
+		var z1 = z0 * zoom_change # next zoom value
+
+		c1 = c0 + (-0.5*v0 + point)*(z0 - z1)
+		camera.zoom = z1
+		camera.global_position = c1
+
+
 func _on_AddStar_pressed():
-	var star = preload("res://scenes/Star.tscn").instance()
-	add_child(star)
-	
+	var star = level.add_star(camera.position)
+	star.monitoring = false
+	star.add_to_group("draggable")
+
+
 func _on_AddPlanet_pressed():
-	var planet = preload("res://scenes/objects/Planet.tscn").instance()
-	add_child(planet)
+	var planet = level.add_object("Planet", camera.position)
+	planet.add_to_group("draggable")
+
+
+
+func _on_Export_pressed():
+	pass
+
+
+func _on_LevelName_text_changed(new_text):
+	var old_name = level_name.text
+	
+	var dir = Directory.new()
+	dir.rename(USER_LEVELS_PATH + old_name + ".json", USER_LEVELS_PATH + old_name + ".json")
+
+
+func _on_Save_pressed():
+	pass # Replace with function body.
+
+
+func _on_Open_pressed():
+	get_tree().paused = true
+	$CanvasLayer/FileDialog.popup_centered()
+	var path = yield($CanvasLayer/FileDialog, "file_selected")
+	var file = File.new()
+	file.open(path, File.READ)
+	var data = parse_json(file.get_as_text())
+	get_tree().paused = false
+	
+	level.load_level(data)
