40838e39b831c7c0c0e2e7ac54a386de115228c1
Dardan Bujupaj <d.bujupaj@bluewin.ch>
2020-11-07 18:30:01 +0100

    Fix level editor references

--- scenes/EditableLevel.gd
+++ scenes/EditableLevel.gd
@@ -1,16 +1,6 @@
 extends "res://scenes/BaseLevel.gd"
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
 # OVERRIDE from BaseLevel
 func add_star(pos: Vector2 = Vector2(0, 0)) -> Node2D:
 	var star = .add_star(pos)
