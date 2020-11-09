ada573d50b77f2d892a2cc17cf45106f9da51ae0
Dardan Bujupaj <d.bujupaj@bluewin.ch>
2020-11-09 15:20:29 +0100

    Add a singleton SoundeEngine to play soundeffects

---
+++ SoundEngine.gd
@@ -0,0 +1,24 @@
+extends Node
+
+const POOL_SIZE = 8
+
+const sounds = {
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for i in range(POOL_SIZE):
+		var player = AudioStreamPlayer.new()
+		add_child(player)
+		
+		
+func _get_idle_player():
+	for player in get_children():
+		if not (player as AudioStreamPlayer).playing:
+			return player
+
+func play_sound(sound_name: String):
+	var audio_player: AudioStreamPlayer = _get_idle_player()
+	audio_player.stream = sounds[sound_name]
+	audio_player.play()
+	
