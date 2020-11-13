fcb063ebafc837767bf264d96227e9647c3e0ee4
gantneba <33327887+gantneba@users.noreply.github.com>
2020-11-13 11:55:16 +0100

    constellation menu
    first iteration of the constellation menu

--- SoundEngine.gd
+++ SoundEngine.gd
@@ -4,6 +4,7 @@ const POOL_SIZE = 8
 
 const sounds = {
 	"MenuButtonSound":preload("res://Sounds/MenuButtonSound.wav"),
+	"MenuButtonHoverSound":preload("res://Sounds/MenuButtonSound.wav"),
 	"MoonImpact":preload("res://Sounds/MoonImpact.wav"),
 	"Wurmhole":preload("res://Sounds/wurmhole.wav"),
 	"Reset":preload("res://Sounds/Reset.wav"),
