01d3b0030156930aa95c6644e79ce6ea75edfd7b
Dardan Bujupaj <d.bujupaj@bluewin.ch>
2020-11-16 15:47:56 +0100

    center level labels. change star connection transition type back to cubic.

--- scenes/title_screen/Constellation.gd
+++ scenes/title_screen/Constellation.gd
@@ -144,12 +144,12 @@ func load_random() -> void:
 func show_lines():
 	tween.stop_all()
 	for line in $lines.get_children():
-		tween.interpolate_property(line, "scale", line.scale, Vector2(1, 1), 1.5, Tween.TRANS_EXPO, Tween.EASE_OUT, 0.0)
+		tween.interpolate_property(line, "scale", line.scale, Vector2(1, 1), 1.5, Tween.TRANS_CUBIC, Tween.EASE_OUT, 0.0)
 	tween.start()
 
 
 func hide_lines():
 	tween.stop_all()
 	for line in $lines.get_children():
-		tween.interpolate_property(line, "scale", line.scale, Vector2(0, 0), 1.5, Tween.TRANS_EXPO, Tween.EASE_OUT, 0.0)
+		tween.interpolate_property(line, "scale", line.scale, Vector2(0, 0), 1.5, Tween.TRANS_CUBIC, Tween.EASE_OUT, 0.0)
 	tween.start()
