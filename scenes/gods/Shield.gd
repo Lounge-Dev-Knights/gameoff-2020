924076306518b723230f76dffd818c9658693de3
Stefan Schneider <mail@stefanschneider.me>
2020-11-24 16:11:54 +0100

    Add spawn and explode animations to mars shield

--- scenes/gods/Shield.gd
+++ scenes/gods/Shield.gd
@@ -1,8 +1,20 @@
 extends Node2D
 
+func reset() -> void:
+	$Swords.modulate = Color(1,1,1,1)
+	$Swords.scale = Vector2(0,0)
 
 func enable() -> void:
+	reset()
 	show()
+	print("spawning shield")
+	print("modulate: ", modulate)
+	$AnimationPlayer.play("spawn")
 	
 func disable() -> void:
+	$AnimationPlayer.play("disappear")
+	yield(get_tree().create_timer(0.5), "timeout")
 	hide()
+	
+
+	
