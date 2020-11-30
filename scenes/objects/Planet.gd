f3a593e739b06f5417cab7e0381af82f6af3ddf1
Dardan Bujupaj <d.bujupaj@bluewin.ch>
2020-11-30 23:24:49 +0100

    add new planet images.

--- scenes/objects/Planet.gd
+++ scenes/objects/Planet.gd
@@ -2,14 +2,22 @@ extends Area2D
 
 var type = "Planet"
 
+const planet_images = [
+	preload("res://scenes/objects/planet_images/planet_1.png"),
+	preload("res://scenes/objects/planet_images/planet_2.png"),
+	preload("res://scenes/objects/planet_images/planet_3.png"),
+	preload("res://scenes/objects/planet_images/planet_4.png"),
+	preload("res://scenes/objects/planet_images/planet_5.png"),
+	preload("res://scenes/objects/planet_images/planet_6.png"),
+	preload("res://scenes/objects/planet_images/planet_7.png"),
+	preload("res://scenes/objects/planet_images/planet_8.png")
+]
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# randomize shader color
-	$texture.material.set_shader_param("base_color", randf())
-	
-	# randomize shader texture
-	$texture.material.set_shader_param("random_modifier", randf())
+	$planet.texture = planet_images[randi() % planet_images.size()]
+	$planet.scale *= $CollisionShape2D.shape.radius / 300
 
 
 func _on_Planet_body_entered(body):
